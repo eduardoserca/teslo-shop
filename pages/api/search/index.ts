51b73a9f72465e8a605b2637ad85229470048da0
Carlos Serrano <carlos.serrano@telefonica.com>
2023-09-05 16:21:05 -0500

    Api products listo

---
+++ pages/api/search/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+type Data = 
+    |{ message: string }
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    return res.status(400).json({
+        message:'Debe especificar un query de búsqueda'
+    })
+
+    
+}
+
