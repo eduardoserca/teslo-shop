fb93d465a6c866d81da450208b4c50d12693dd3b
Carlos Serrano <carlos.serrano@telefonica.com>
2023-09-19 11:40:36 -0500

    revertiendo los cambios baseURL

--- api/tesloApi.ts
+++ api/tesloApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const tesloApi = axios.create({
-    //baseURL:'/api'
-    baseURL:'https://teslo-shop-delta-nine.vercel.app/api'
+    baseURL:'/api'
+    //baseURL:'https://teslo-shop-delta-nine.vercel.app/api'
 });
 
 export default tesloApi;
