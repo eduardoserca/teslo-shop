3c1f425be85b061a7f026a6d0f86ed4df054c506
Carlos Serrano <carlos.serrano@telefonica.com>
2023-09-19 11:36:27 -0500

    Modificando baseURL de Axios con diminio de Prod

--- api/tesloApi.ts
+++ api/tesloApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const tesloApi = axios.create({
-    baseURL:'/api'
+    //baseURL:'/api'
+    baseURL:'https://teslo-shop-delta-nine.vercel.app/api'
 });
 
 export default tesloApi;
